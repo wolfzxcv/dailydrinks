3bef9168a63f4f06fdf6d667d32a466bfa271020
Nien <wolfzxcv@gmail.com>
2019-12-04 22:21:19 +0800

    Updates

---
+++ precache-manifest.c59817a06754a15c7fed8d8bf6263ff7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcd02f17df8235ca7d2505750bdd5dff",
+    "url": "/dailydrinks/index.html"
+  },
+  {
+    "revision": "eb1b0f2f524f8d4f87d2",
+    "url": "/dailydrinks/static/css/main.14791b58.chunk.css"
+  },
+  {
+    "revision": "c3cc7a4cff94ddef2e0a",
+    "url": "/dailydrinks/static/js/2.8dcb9920.chunk.js"
+  },
+  {
+    "revision": "eb1b0f2f524f8d4f87d2",
+    "url": "/dailydrinks/static/js/main.5a2c3d6a.chunk.js"
+  },
+  {
+    "revision": "684f6a883d7d2f96cb52",
+    "url": "/dailydrinks/static/js/runtime-main.dd40e46d.js"
+  }
+]);
